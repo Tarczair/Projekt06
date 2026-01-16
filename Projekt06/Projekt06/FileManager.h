977c7f4889244a3b74daebd09389e24dd433565d
Tarczair <97432438+Tarczair@users.noreply.github.com>
2026-01-16 14:40:23 +0100

    poprawa z czytaniem plikow

--- Projekt06/Projekt06/FileManager.h
+++ Projekt06/Projekt06/FileManager.h
@@ -1,4 +1,3 @@
-#pragma once
 #ifndef FILEMANAGER_H
 #define FILEMANAGER_H
 
@@ -6,6 +5,10 @@
 #include <string>
 
 class FileManager {
+private:
+    // Pomocnicza metoda do usuwania cudzys³owów z pól CSV
+    static std::string cleanQuotes(std::string str);
+
 public:
     static void loadCSV(EnergyTree& tree, const std::string& filename);
     static void saveBinary(const EnergyTree& tree, const std::string& filename);
